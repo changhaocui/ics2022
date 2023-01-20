65f9c6a57c946131eb8ea4d605d892028dfb7719
tracer-ics2022 <tracer@njuics.org>
2023-01-20 19:58:03 +0800

    >  compile NEMU 211220000 李四 Linux cui-VirtualBox 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:58:03 up  7:23,  1 user,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -235,7 +235,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   for (int i = 0; i < nr_token; i ++) {
     if (tokens[i].type == '*' && (i == 0 || (tokens[i - 1].type != TK_NUM && tokens[i - 1].type != TK_REG) )) {
-      printf("将*号转变为解引用");
+      printf("将*号转变为解引用%d\n",i);
       tokens[i].type = TK_DEREF ;
     }
   }
