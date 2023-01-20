831702d260d5093c68388b1f812cb249d124206c
tracer-ics2022 <tracer@njuics.org>
2023-01-20 19:55:01 +0800

    >  compile NEMU 211220000 李四 Linux cui-VirtualBox 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:55:01 up  7:20,  1 user,  load average: 0.04, 0.01, 0.00

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -32,3 +32,14 @@ void isa_reg_display() {
     printf("%-8s%-#20x%-20d\n", regs[i],(unsigned int) cpu.gpr[i],(int) cpu.gpr[i]);
   }
 }
+word_t isa_reg_str2val(const char *s, bool *success) {
+  int reg_num = ARRLEN(regs);
+  int i;
+  for (i = 0; i < reg_num; i++) {
+    if(regs[i] == s){
+      return cpu.gpr[i];
+    }
+  }
+  printf("寄存器不存在");
+  assert(0);
+}
