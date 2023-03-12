85392b54b9a94d336e32e0b1b35da2dfa4f1c6c5
tracer-ics2022 <tracer@njuics.org>
2023-03-12 09:55:46 +0800

    >  compile NEMU 211220000 李四 Linux cui-VirtualBox 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:55:46 up  1:06,  1 user,  load average: 0.09, 0.05, 0.01

--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -8,16 +8,7 @@
 #include <elf.h>
 
 #define MAX_IRINGBUF 16
-#define ftrace_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
-  do { \
-    extern FILE* log_fp; \
-    extern bool log_enable(); \
-    if (log_enable()) { \
-      fprintf(log_fp, __VA_ARGS__); \
-      fflush(log_fp); \
-    } \
-  } while (0) \
-)
+
 
 Decode* iringbuf[MAX_IRINGBUF];
 int p_cur = 0;
@@ -87,174 +78,174 @@ static void read_elf_header(int fd, Elf64_Ehdr *eh) {
 
 static void display_elf_hedaer(Elf64_Ehdr eh) {
 	/* Storage capacity class */
-	ftrace_write("Storage class\t= ");
+	log_write("Storage class\t= ");
 	switch(eh.e_ident[EI_CLASS])
 	{
 		case ELFCLASS32:
-			ftrace_write("32-bit objects\n");
+			log_write("32-bit objects\n");
 			break;
 
 		case ELFCLASS64:
-			ftrace_write("64-bit objects\n");
+			log_write("64-bit objects\n");
 			break;
 
 		default:
-			ftrace_write("INVALID CLASS\n");
+			log_write("INVALID CLASS\n");
 			break;
 	}
 
 	/* Data Format */
-	ftrace_write("Data format\t= ");
+	log_write("Data format\t= ");
 	switch(eh.e_ident[EI_DATA])
 	{
 		case ELFDATA2LSB:
-			ftrace_write("2's complement, little endian\n");
+			log_write("2's complement, little endian\n");
 			break;
 
 		case ELFDATA2MSB:
-			ftrace_write("2's complement, big endian\n");
+			log_write("2's complement, big endian\n");
 			break;
 
 		default:
-			ftrace_write("INVALID Format\n");
+			log_write("INVALID Format\n");
 			break;
 	}
 
 	/* OS ABI */
-	ftrace_write("OS ABI\t\t= ");
+	log_write("OS ABI\t\t= ");
 	switch(eh.e_ident[EI_OSABI])
 	{
 		case ELFOSABI_SYSV:
-			ftrace_write("UNIX System V ABI\n");
+			log_write("UNIX System V ABI\n");
 			break;
 
 		case ELFOSABI_HPUX:
-			ftrace_write("HP-UX\n");
+			log_write("HP-UX\n");
 			break;
 
 		case ELFOSABI_NETBSD:
-			ftrace_write("NetBSD\n");
+			log_write("NetBSD\n");
 			break;
 
 		case ELFOSABI_LINUX:
-			ftrace_write("Linux\n");
+			log_write("Linux\n");
 			break;
 
 		case ELFOSABI_SOLARIS:
-			ftrace_write("Sun Solaris\n");
+			log_write("Sun Solaris\n");
 			break;
 
 		case ELFOSABI_AIX:
-			ftrace_write("IBM AIX\n");
+			log_write("IBM AIX\n");
 			break;
 
 		case ELFOSABI_IRIX:
-			ftrace_write("SGI Irix\n");
+			log_write("SGI Irix\n");
 			break;
 
 		case ELFOSABI_FREEBSD:
-			ftrace_write("FreeBSD\n");
+			log_write("FreeBSD\n");
 			break;
 
 		case ELFOSABI_TRU64:
-			ftrace_write("Compaq TRU64 UNIX\n");
+			log_write("Compaq TRU64 UNIX\n");
 			break;
 
 		case ELFOSABI_MODESTO:
-			ftrace_write("Novell Modesto\n");
+			log_write("Novell Modesto\n");
 			break;
 
 		case ELFOSABI_OPENBSD:
-			ftrace_write("OpenBSD\n");
+			log_write("OpenBSD\n");
 			break;
 
 		case ELFOSABI_ARM_AEABI:
-			ftrace_write("ARM EABI\n");
+			log_write("ARM EABI\n");
 			break;
 
 		case ELFOSABI_ARM:
-			ftrace_write("ARM\n");
+			log_write("ARM\n");
 			break;
 
 		case ELFOSABI_STANDALONE:
-			ftrace_write("Standalone (embedded) app\n");
+			log_write("Standalone (embedded) app\n");
 			break;
 
 		default:
-			ftrace_write("Unknown (0x%x)\n", eh.e_ident[EI_OSABI]);
+			log_write("Unknown (0x%x)\n", eh.e_ident[EI_OSABI]);
 			break;
 	}
 
 	/* ELF filetype */
-	ftrace_write("Filetype \t= ");
+	log_write("Filetype \t= ");
 	switch(eh.e_type)
 	{
 		case ET_NONE:
-			ftrace_write("N/A (0x0)\n");
+			log_write("N/A (0x0)\n");
 			break;
 
 		case ET_REL:
-			ftrace_write("Relocatable\n");
+			log_write("Relocatable\n");
 			break;
 
 		case ET_EXEC:
-			ftrace_write("Executable\n");
+			log_write("Executable\n");
 			break;
 
 		case ET_DYN:
-			ftrace_write("Shared Object\n");
+			log_write("Shared Object\n");
 			break;
 		default:
-			ftrace_write("Unknown (0x%x)\n", eh.e_type);
+			log_write("Unknown (0x%x)\n", eh.e_type);
 			break;
 	}
 
 	/* ELF Machine-id */
-	ftrace_write("Machine\t\t= ");
+	log_write("Machine\t\t= ");
 	switch(eh.e_machine)
 	{
 		case EM_NONE:
-			ftrace_write("None (0x0)\n");
+			log_write("None (0x0)\n");
 			break;
 
 		case EM_386:
-			ftrace_write("INTEL x86 (0x%x)\n", EM_386);
+			log_write("INTEL x86 (0x%x)\n", EM_386);
 			break;
 
 		case EM_X86_64:
-			ftrace_write("AMD x86_64 (0x%x)\n", EM_X86_64);
+			log_write("AMD x86_64 (0x%x)\n", EM_X86_64);
 			break;
 
 		case EM_AARCH64:
-			ftrace_write("AARCH64 (0x%x)\n", EM_AARCH64);
+			log_write("AARCH64 (0x%x)\n", EM_AARCH64);
 			break;
 
 		default:
-			ftrace_write(" 0x%x\n", eh.e_machine);
+			log_write(" 0x%x\n", eh.e_machine);
 			break;
 	}
 
 	/* Entry point */
-	ftrace_write("Entry point\t= 0x%08lx\n", eh.e_entry);
+	log_write("Entry point\t= 0x%08lx\n", eh.e_entry);
 
 	/* ELF header size in bytes */
-	ftrace_write("ELF header size\t= 0x%08x\n", eh.e_ehsize);
+	log_write("ELF header size\t= 0x%08x\n", eh.e_ehsize);
 
 	/* Program Header */
-	ftrace_write("Program Header\t= ");
-	ftrace_write("0x%08lx\n", eh.e_phoff);		/* start */
-	ftrace_write("\t\t  %d entries\n", eh.e_phnum);	/* num entry */
-	ftrace_write("\t\t  %d bytes\n", eh.e_phentsize);	/* size/entry */
+	log_write("Program Header\t= ");
+	log_write("0x%08lx\n", eh.e_phoff);		/* start */
+	log_write("\t\t  %d entries\n", eh.e_phnum);	/* num entry */
+	log_write("\t\t  %d bytes\n", eh.e_phentsize);	/* size/entry */
 
 	/* Section header starts at */
-	ftrace_write("Section Header\t= ");
-	ftrace_write("0x%08lx\n", eh.e_shoff);		/* start */
-	ftrace_write("\t\t  %d entries\n", eh.e_shnum);	/* num entry */
-	ftrace_write("\t\t  %d bytes\n", eh.e_shentsize);	/* size/entry */
-	ftrace_write("\t\t  0x%08x (string table offset)\n", eh.e_shstrndx);
+	log_write("Section Header\t= ");
+	log_write("0x%08lx\n", eh.e_shoff);		/* start */
+	log_write("\t\t  %d entries\n", eh.e_shnum);	/* num entry */
+	log_write("\t\t  %d bytes\n", eh.e_shentsize);	/* size/entry */
+	log_write("\t\t  0x%08x (string table offset)\n", eh.e_shstrndx);
 
 	/* File flags (Machine specific)*/
-	ftrace_write("File flags \t= 0x%08x\n", eh.e_flags);
+	log_write("File flags \t= 0x%08x\n", eh.e_flags);
 
 	/* ELF file flags are machine specific.
 	 * INTEL implements NO flags.
@@ -262,50 +253,50 @@ static void display_elf_hedaer(Elf64_Ehdr eh) {
 	 * Add support below to parse ELF file flags on ARM
 	 */
 	int32_t ef = eh.e_flags;
-	ftrace_write("\t\t  ");
+	log_write("\t\t  ");
 
 	if(ef & EF_ARM_RELEXEC)
-		ftrace_write(",RELEXEC ");
+		log_write(",RELEXEC ");
 
 	if(ef & EF_ARM_HASENTRY)
-		ftrace_write(",HASENTRY ");
+		log_write(",HASENTRY ");
 
 	if(ef & EF_ARM_INTERWORK)
-		ftrace_write(",INTERWORK ");
+		log_write(",INTERWORK ");
 
 	if(ef & EF_ARM_APCS_26)
-		ftrace_write(",APCS_26 ");
+		log_write(",APCS_26 ");
 
 	if(ef & EF_ARM_APCS_FLOAT)
-		ftrace_write(",APCS_FLOAT ");
+		log_write(",APCS_FLOAT ");
 
 	if(ef & EF_ARM_PIC)
-		ftrace_write(",PIC ");
+		log_write(",PIC ");
 
 	if(ef & EF_ARM_ALIGN8)
-		ftrace_write(",ALIGN8 ");
+		log_write(",ALIGN8 ");
 
 	if(ef & EF_ARM_NEW_ABI)
-		ftrace_write(",NEW_ABI ");
+		log_write(",NEW_ABI ");
 
 	if(ef & EF_ARM_OLD_ABI)
-		ftrace_write(",OLD_ABI ");
+		log_write(",OLD_ABI ");
 
 	if(ef & EF_ARM_SOFT_FLOAT)
-		ftrace_write(",SOFT_FLOAT ");
+		log_write(",SOFT_FLOAT ");
 
 	if(ef & EF_ARM_VFP_FLOAT)
-		ftrace_write(",VFP_FLOAT ");
+		log_write(",VFP_FLOAT ");
 
 	if(ef & EF_ARM_MAVERICK_FLOAT)
-		ftrace_write(",MAVERICK_FLOAT ");
+		log_write(",MAVERICK_FLOAT ");
 
-	ftrace_write("\n");
+	log_write("\n");
 
 	/* MSB of flags conatins ARM EABI version */
-	ftrace_write("ARM EABI\t= Version %d\n", (ef & EF_ARM_EABIMASK)>>24);
+	log_write("ARM EABI\t= Version %d\n", (ef & EF_ARM_EABIMASK)>>24);
 
-	ftrace_write("\n");	/* End of ELF header */
+	log_write("\n");	/* End of ELF header */
 }
 
 static void read_section(int fd, Elf64_Shdr sh, void *dst) {
@@ -328,27 +319,27 @@ static void display_section_headers(int fd, Elf64_Ehdr eh, Elf64_Shdr sh_tbl[]) 
   
 	/* Read section-header string-table */
 
-	ftrace_write("========================================");
-	ftrace_write("========================================\n");
-	ftrace_write(" idx offset     load-addr  size       algn"
+	log_write("========================================");
+	log_write("========================================\n");
+	log_write(" idx offset     load-addr  size       algn"
 			" flags      type       section\n");
-	ftrace_write("========================================");
-	ftrace_write("========================================\n");
+	log_write("========================================");
+	log_write("========================================\n");
 
 	for(int i = 0; i < eh.e_shnum; i++) {
-		ftrace_write(" %03d ", i);
-		ftrace_write("0x%08lx ", sh_tbl[i].sh_offset);
-		ftrace_write("0x%08lx ", sh_tbl[i].sh_addr);
-		ftrace_write("0x%08lx ", sh_tbl[i].sh_size);
-		ftrace_write("%-4ld ", sh_tbl[i].sh_addralign);
-		ftrace_write("0x%08lx ", sh_tbl[i].sh_flags);
-		ftrace_write("0x%08x ", sh_tbl[i].sh_type);
-		ftrace_write("%s\t", (sh_str + sh_tbl[i].sh_name));
-		ftrace_write("\n");
+		log_write(" %03d ", i);
+		log_write("0x%08lx ", sh_tbl[i].sh_offset);
+		log_write("0x%08lx ", sh_tbl[i].sh_addr);
+		log_write("0x%08lx ", sh_tbl[i].sh_size);
+		log_write("%-4ld ", sh_tbl[i].sh_addralign);
+		log_write("0x%08lx ", sh_tbl[i].sh_flags);
+		log_write("0x%08x ", sh_tbl[i].sh_type);
+		log_write("%s\t", (sh_str + sh_tbl[i].sh_name));
+		log_write("\n");
 	}
-	ftrace_write("========================================");
-	ftrace_write("========================================\n");
-	ftrace_write("\n");	/* end of section header table */
+	log_write("========================================");
+	log_write("========================================\n");
+	log_write("\n");	/* end of section header table */
 }
 
 static void read_symbol_table(int fd, Elf64_Ehdr eh, Elf64_Shdr sh_tbl[], int sym_idx) {
@@ -361,12 +352,12 @@ static void read_symbol_table(int fd, Elf64_Ehdr eh, Elf64_Shdr sh_tbl[], int sy
   
   int sym_count = (sh_tbl[sym_idx].sh_size / sizeof(Elf64_Sym));
 	// log
-  ftrace_write("Symbol count: %d\n", sym_count);
-  ftrace_write("====================================================\n");
-  ftrace_write(" num    value            type size       name\n");
-  ftrace_write("====================================================\n");
+  log_write("Symbol count: %d\n", sym_count);
+  log_write("====================================================\n");
+  log_write(" num    value            type size       name\n");
+  log_write("====================================================\n");
   for (int i = 0; i < sym_count; i++) {
-    ftrace_write(" %-3d    %016lx %-4d %-10ld %s\n",
+    log_write(" %-3d    %016lx %-4d %-10ld %s\n",
       i,
       sym_tbl[i].st_value, 
       ELF64_ST_TYPE(sym_tbl[i].st_info),
@@ -374,7 +365,7 @@ static void read_symbol_table(int fd, Elf64_Ehdr eh, Elf64_Shdr sh_tbl[], int sy
       str_tbl + sym_tbl[i].st_name
     );
   }
-  ftrace_write("====================================================\n\n");
+  log_write("====================================================\n\n");
 
 	// read
 	symbol_tbl_size = sym_count;
@@ -462,7 +453,7 @@ void trace_func_call(paddr_t pc, paddr_t target, bool is_tail) {
 	if (call_depth <= 2) return; // ignore _trm_init & main
 
 	int i = find_symbol_func(target, true);
-	ftrace_write(FMT_PADDR ": %*scall [%s@" FMT_PADDR "]\n",
+	log_write(FMT_PADDR ": %*scall [%s@" FMT_PADDR "]\n",
 		pc,
 		(call_depth-3)*2, "",
 		i>=0?symbol_tbl[i].name:"???",
@@ -480,7 +471,7 @@ void trace_func_ret(paddr_t pc) {
 	if (call_depth <= 2) return; // ignore _trm_init & main
 
 	int i = find_symbol_func(pc, false);
-	ftrace_write(FMT_PADDR ": %*sret [%s]\n",
+	log_write(FMT_PADDR ": %*sret [%s]\n",
 		pc,
 		(call_depth-3)*2, "",
 		i>=0?symbol_tbl[i].name:"???"
