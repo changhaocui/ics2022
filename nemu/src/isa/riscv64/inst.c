465fd2dfc5c4aa8060bbbdd8b948c4b5ebf348fd
tracer-ics2022 <tracer@njuics.org>
2023-03-13 21:46:46 +0800

    >  compile NEMU 211220000 李四 Linux cui-VirtualBox 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:46:46 up  1:57,  1 user,  load average: 0.08, 0.03, 0.01

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -40,7 +40,8 @@ enum {
 SEXT(BITS(i, 30, 21), 10) << 1 |\
 SEXT(BITS(i, 20, 20), 1) << 11 |\
 SEXT(BITS(i, 19, 12), 8) << 12 ; } while(0)
-#define immB() do { *imm = (SEXT(BITS(i, 31, 30), 1) << 12) | (SEXT(BITS(i, 30, 25), 6) << 5) | (SEXT(BITS(i, 11, 8), 4) << 1) | (SEXT(BITS(i, 8, 7), 1) << 11); } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 30), 1) << 12) | (SEXT(BITS(i, 30, 25), 6) << 5) \
+| (SEXT(BITS(i, 11, 8), 4) << 1) | (SEXT(BITS(i, 8, 7), 1) << 11); } while(0)
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
   //译码结果将记录到函数参数dest, src1, src2和imm中, 它们分别代表目的操作数, 两个源操作数和立即数.
   uint32_t i = s->isa.inst.val;
@@ -55,7 +56,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;//自己加的
-    case TYPE_B:                   immB(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
   }
 }
 
